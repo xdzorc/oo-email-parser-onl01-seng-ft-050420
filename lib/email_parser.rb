dfe5343c3d8cd7145c7b48caa0db1511de340e49
DAZUN XING <dazun.xing@gmail.com>
2020-05-17 10:04:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,16 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+  attr_accessor :address
+  @@address =[]
+  def initialize(address)
+  @@address << address.split(/[ ,]+/)
+  end
+  
+  def parse
+   a=@@address.uniq.flatten!
+   a
+     
+  end
+end
